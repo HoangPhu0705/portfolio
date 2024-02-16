6548daccc771e9ff876b9ce380769fc195f89566
HoangPhu0705 <phuhoang07051003@gmail.com>
2024-02-16 15:41:36 +0700

    portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,165 @@
+import React from "react";
+import Tilt from 'react-parallax-tilt';
+import { motion } from "framer-motion";
+import {logo_color, menu, close, avatar} from '../assets'
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant, slideIn } from "../utils/motion";
+import { UserCard } from 'react-ui-cards';
+
+const InfoCard = () => {
+  return (
+
+    <Tilt
+      className = "xs:w-[250px] w-full"
+    >
+    <motion.div
+        variants={fadeIn("left", "spring")}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 250,
+          }}
+          className="relative bg-tertiary rounded-[20px]
+            py-5 px-12 min-h-[350px] flex justify-evenly 
+            items-center flex-col"
+          > 
+            <img src= {logo_color} alt="title" 
+              className="w-24 h-24 object-contain rounded-2xl"
+            />
+
+
+            <div className="">
+              <h3 className="text-secondary text-[16px] font-bold text-center mt-2">
+                  Phan Hoang Phu
+              </h3>
+              <p className="text-secondary text-[14px] text-center mt-2">Software Engineer</p>
+
+            </div>
+
+           
+
+            <div
+              className="absolute bg-white/80 h-full w-full flex items-center justify-center opacity-0 rounded-2xl hover:opacity-100">
+
+              <button className="flex flex-col justify-center items-center w-full h-full">
+                <img src= "https://img.icons8.com/carbon-copy/100/download.png" alt="title" 
+                  className="w-16 h-16 object-contain rounded-full bg-white"
+                />
+                <p className="text-primary font-black">Download CV</p>
+              </button>
+
+              
+            </div>
+
+          </div>
+      </motion.div>
+
+
+
+    </Tilt>
+
+
+  )
+}
+
+
+
+const ServiceCard = ({index, title, icon}) => {
+  return (
+    <Tilt className = "xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px]
+            py-5 px-12 min-h-[280px] flex justify-evenly 
+            items-center flex-col"
+          > 
+            <img src={icon} alt="title" 
+              className="w-16 h-16 object-contain"
+            />
+
+            <h3 className="text-white text-[20px] font-bold text-center">
+              {title}
+            </h3>
+
+          </div>
+      </motion.div>
+
+    </Tilt>
+  )
+  
+}
+
+const About = () => {
+  return (
+    <>
+      <div className="flex flex-wrap items-center justify-between ">
+        <div className="flex flex-col">
+          <motion.div
+            variants={textVariant()}
+          >
+            <p className={styles.sectionSubText}>Introduction</p>
+            <h2 className={styles.sectionHeadText}>Overview.</h2>
+
+
+          </motion.div>
+
+          <motion.p
+            variants={fadeIn("", "", 0.1, 1)}
+            className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+          >
+            As a college student, I am actively developing my skills in softwares development. 
+            I have gained some experience in working with frameworks like React, Flutter, NodeJS, ASP.NET and SpringBoot.
+            I am enthusiastic about learning and have a strong desire to collaborate closely with clients 
+            to create efficient, scalable, and user-friendly solutions that address real-world problems. 
+            I am eager to work together with you to transform your ideas into reality and contribute to meaningful projects.
+          </motion.p>
+        </div>
+
+
+        <InfoCard />
+          
+          
+
+        
+            
+      
+
+
+
+
+      </div>
+
+
+      
+
+      <div className="mt-20 flex flex-wrap justify-between gap-5">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+
+
+      </div>
+
+
+    </>
+
+
+
+
+  )
+}
+
+export default SectionWrapper(About, "about");
---
+++ src/constants/index.js
@@ -0,0 +1,232 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tdt,
+    tesla,
+    shopify,
+    carrent,
+    cookie,
+    calltel,
+    malefashion,
+    jobit,
+    tripguide,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "Mobile Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Content Creator",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "",
+      company_name: "Ton Duc Thang University",
+      icon: tdt,
+      iconBg: "#383E56",
+      date: "October 2021 - Now",
+      points: [
+        "Learning fundamental programming concepts and data structures.",
+        "Understanding algorithms and their complexity analysis.",
+        "Developing software applications using various programming languages such as Java, C++, or Python.",
+        "Working with databases and learning SQL for data manipulation and retrieval.",
+        "Gaining knowledge of software engineering principles and best practices.",
+        "Collaborating with peers on group projects to develop real-world software solutions.",
+        "Learning about software testing and debugging techniques to ensure the quality of applications.",
+      ],
+    },
+  ];
+  const testimonials = [
+    {
+      testimonial:
+        "When you have a dream, you've got to grab it and never let go",
+      name: "Carol Burnett",  
+      designation: "",
+      company: "",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyPhEeUo6GGHpnU-ea5VPj-sQxsCmzLrSAzw&usqp=CAU",
+    },
+    {
+      testimonial:
+        "A person who never made a mistake never tried anything new.",
+      name: "Albert Einstein",
+      designation: "",
+      company: "",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Albert_Einstein_Head.jpg/450px-Albert_Einstein_Head.jpg",
+    },
+    // {
+    //   testimonial:
+    //     "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    //   name: "Lisa Wang",
+    //   designation: "CTO",
+    //   company: "456 Enterprises",
+    //   image: "https://randomuser.me/api/portraits/women/6.jpg",
+    // },
+  ];
+  
+  const projects = [
+    {
+      name: "Cookie",
+      description:
+        "An English learning app built using the Flutter framework and Firebase, is a versatile app available across multiple platforms",
+      tags: [
+        {
+          name: "flutter",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "dart",
+          color: "green-text-gradient",
+        },
+        {
+          name: "firebase",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: cookie,
+      source_code_link: "https://github.com/HoangPhu0705/cookie.git",
+    },
+    {
+      name: "CallTel",
+      description:
+        "CallTel is a robust web-based point of sale (POS) application designed specifically for phone store management.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "nodejs",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: calltel,
+      source_code_link: "https://github.com/HoangPhu0705/nodejs_callTel",
+    },
+    {
+      name: "Male Fashion",
+      description:
+        "MaleFashion is a dynamic .NET Core web application designed to be the go-to platform for all fashion enthusiasts.",
+      tags: [
+        {
+          name: ".NET Core",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "sqlserver",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: malefashion,
+      source_code_link: "https://github.com/HoangPhu0705/malefashion.git",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
